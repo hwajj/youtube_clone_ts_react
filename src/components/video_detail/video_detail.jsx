e4ac409f9cf13fad4e48b41cb92410f42f83145e
hwajj <azamwara@gmail.com>
2022-10-02 17:24:20 +0900

    Add video iframe

--- src/components/video_detail/video_detail.jsx
+++ src/components/video_detail/video_detail.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import styles from './video_detail.module.css';
-const VideoDetail = ({ video }) => {
-  console.log(video);
+const VideoDetail = ({ video: { snippet }, video }) => (
+  <section className={styles.detail}>
+    <iframe
+      className={styles.video}
+      id='ytplayer'
+      type='text/html'
+      width='100%'
+      height='500px'
+      src={`https://www.youtube.com/embed/${video.id}`}
+      frameBorder='0'
+      allowFullScreen
+    ></iframe>
 
-  return <h1>{video.snippet.title}</h1>;
-};
+    <h2>{video.snippet.title}</h2>
+    <h3>{snippet.channelTitle}</h3>
+    <pre className={styles.description}>{snippet.description}</pre>
+  </section>
+);
 
 export default VideoDetail;
