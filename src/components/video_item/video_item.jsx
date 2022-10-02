281dd703b5b8ff5967f92cfbe6c9418de53ea5d7
hwajj <azamwara@gmail.com>
2022-10-02 17:38:36 +0900

    Remove console.log

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import styles from './video_item.module.css';
 const VideoItem = ({ video, onVideoClick, video: { snippet }, display }) => {
-  console.log(video);
-  console.log(display);
   const displayType = display === 'list' ? styles.list : styles.grid;
   return (
     <li
